71be009f383e0457840395b1006d968d7125704b
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-12-14 21:35:13 +0300

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,8 +1,5 @@
 # Read input n
 n = int(input())
 
-# If n is 1, the last two digits of 5^1 are 05, otherwise they are 25 for n >= 2
-if n == 1:
-    print(5)  # Last two digits of 5^1
-else:
-    print(25)  # Last two digits of 5^n for n >= 2
+# Since the problem states n >= 2, we directly print 25 for any n >= 2
+print(25)
